5ce6bb0dcbd8c34d3e570265a5931aca94ba5fe0
Ronak Patel <ronakp@whackbook.myfiosgateway.com>
2023-02-01 21:44:52 -0500

    CSV Combiner Program: Updated version now works for both same and diff column CSV files

--- csv-combiner/csv-combiner.php
+++ csv-combiner/csv-combiner.php
@@ -19,41 +19,63 @@ for ($i = 1; $i < $argc; $i++) {
 }
 
 # Read/Write each CSV file into an array
-# Note: may not be the most memory-efficient way to go about this
-$fileContents = [];
+# Note: 
+
+# Note: had to copy full path due to some errors - change as necessary
+$path = "/Users/ronakp/Desktop/PMG/ProgrammingChallenges/csv-combiner/fixtures/";
+
+$combinedCSV = [];
+$headers = [];
+$rowCount = 0;
 foreach ($fileNames as $fName) {
-    # Note: had to copy full path due to some errors - change as necessary
-    $path = "/Users/ronakp/Desktop/PMG/ProgrammingChallenges/csv-combiner/fixtures/";
 
     $f = fopen($path.$fName, 'r');
     if ($f === false) {
         exit("Unable to open file: ".$fName);
     }
 
-    $fileContents[$fName] = [];
+    $i = 0;
+    # Note: declared tempHeaders here since it is used in an if-else branch 
+    $tempHeaders = [];
     while (($row = fgetcsv($f)) !== false) {
         $fileContents[$fName][] = $row;
+        
+        if ($i == 0) {
+            # Header row - add to total headers and record temp headers
+            $headers = array_unique(array_merge($headers, $row));
+            $tempHeaders = $row;
+        }
+        else {
+            # Create/Translate row data
+            $combinedCSV['filename'][] = $fName;
+
+            foreach ($row as $index => $value) {
+                if (in_array($tempHeaders[$index], array_keys($combinedCSV))) {
+                    $combinedCSV[$tempHeaders[$index]][] = $value;
+                }
+                else {
+                    $nils = 0;
+                    while ($nils < $rowCount) {
+                        $combinedCSV[$tempHeaders[$index]][] = "NIL";
+                        $nils += 1;
+                    }
+                    $combinedCSV[$tempHeaders[$index]][] = $value;
+                }
+                #Find leftover columns to add to
+                foreach ($headers as $h) {
+                    if (! in_array($h, $tempHeaders)) {
+                        $combinedCSV[$h][] = "NIL";
+                    }
+                }
+            }
+
+            $rowCount += 1;
+        }
+
+        $i += 1;
     }
 
     fclose($f);
-}
-
-# DESCRIPTION COMMENT
-# Note: assumes files are not empty?? 
-$combinedCSV = [];
-# Iterating through each file and retrieving filename
-foreach ($fileContents as $file => $content) {
-    # Iterating through each row of a file's contents
-    $iCount = count($content);
-    for ($i = 1; $i < $iCount; $i++) {
-        #record filename
-        $combinedCSV['filename'][] = $file;
-        #iterate through/record each element in row
-        $jCount = count($content[$i]);
-        for ($j = 0; $j < $jCount; $j++) {
-            $combinedCSV[$content[0][$j]][] = $content[$i][$j];
-        }
-    }
 }
 
 # Write data as CSV to stdout
@@ -61,9 +83,8 @@ $fstdout = fopen("php://stdout", 'w');
 
 $headers = array_keys($combinedCSV);
 fputcsv($fstdout, $headers);
-#HARD INDEXING - change later
-$iCount = count($combinedCSV[$headers[0]]);
 
+$iCount = count($combinedCSV[$headers[0]]);
 for ($i = 0; $i < $iCount; $i++) {
     $row = [];
     foreach ($headers as $h) {
